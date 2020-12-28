af4c3d38de0cf8bc65036aa106013715749cae71
cowjuh <jenzhang530@gmail.com>
2020-12-27 21:57:40 -0700

    Fixed server

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,31 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const publicPath = path.join(__dirname + 'public');
+const publicPath = path.join(__dirname, '../', '/build');
 const uri = process.env.ATLAS_URI;
 const notesRouter = require('./routes/notes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
-});
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-);
+app.use('/notes', notesRouter);
+
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, '/index.html'));
+});
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
-})
 
-app.use('/notes', notesRouter);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
