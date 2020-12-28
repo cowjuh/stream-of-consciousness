9026e9f0d4c4c02146fe4ca4af73feb05254bdd3
cowjuh <jenzhang530@gmail.com>
2020-12-27 19:41:13 -0700

    Many updates

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const publicPath = path.join(__dirname, '..', 'public');
 const uri = process.env.ATLAS_URI;
 const notesRouter = require('./routes/notes');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.use(cors());
 app.use(bodyParser.json());
