22095b8c0712d1ecba33b1a5ca9bc0027d1fdf07
Jenny Zhang <jenzhang530@gmail.com>
2021-12-21 15:04:14 -0700

    Enable filtering feature

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,63 +1,64 @@
-const router = require('express').Router();
-let Note = require('../models/note.model');
+const router = require("express").Router();
+let Note = require("../models/note.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   Note.find()
-  .then(notes => res.json(notes))
-  .catch(err => res.status(400).json('Error: ' + err));    
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/user/:id').get((req, res) => {
-  Note.find({userID: req.params.id})
-  .then(notes => res.json(notes))
-  .catch(err => res.status(400).json('Error: ' + err));    
+router.route("/user/:id").get((req, res) => {
+  Note.find({ userID: req.params.id })
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/filterByTag/user/:id').get((req, res) => {
-  let tagQuery = req.query.tag;
+router.route("/filterByTag/:id").get((req, res) => {
   let id = req.params.id;
+  let tagQuery = req.query.tag;
+  let tagQuerySplit = tagQuery.split(",");
   let categoryQuery = req.query.category;
-  if (tagQuery.length != 0 && (!categoryQuery || categoryQuery==="All")) {
-    Note.find({tags: {$all: tagQuery.split(',')}}, {userID: id})
-    .then(notes => res.json(notes))
-    .catch(err => res.status(400).json('Error: ' + err));    
-  }
-  else if (tagQuery.length != 0 && categoryQuery) {
-    Note.find(
-      {
-        tags: {$all: tagQuery.split(',')},
-        category: categoryQuery
+  if (tagQuery.length != 0 && (!categoryQuery || categoryQuery === "All")) {
+    Note.find({
+      tags: { $all: tagQuerySplit },
+      userID: id,
     })
-    .then(notes => res.json(notes))
-    .catch(err => res.status(400).json('Error: ' + err));  
+      .then((notes) => res.json(notes))
+      .catch((err) => res.status(400).json("Error: " + err));
+  } else if (tagQuery.length != 0 && categoryQuery) {
+    Note.find({
+      tags: { $all: tagQuerySplit },
+      category: categoryQuery,
+      userID: id,
+    })
+      .then((notes) => res.json(notes))
+      .catch((err) => res.status(400).json("Error: " + err));
+  } else {
+    Note.find({ userID: id })
+      .then((notes) => res.json(notes))
+      .catch((err) => res.status(400).json("Error: " + err));
   }
-  else {
-    Note.find()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(400).json('Error: ' + err)); 
-  }
 });
 
-router.route('/tags/user/:id').get((req, res) => {
-  Note.distinct("tags", {"userID": req.params.id})
-  .then(notes => res.json(notes))
-  .catch(err => res.status(400).json('Error: ' + err));    
+router.route("/tags/user/:id").get((req, res) => {
+  Note.distinct("tags", { userID: req.params.id })
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/categories/user/:id').get((req, res) => {
-  Note.distinct("category", {"userID": req.params.id})
-  .then(notes => res.json(notes))
-  .catch(err => res.status(400).json('Error: ' + err));    
+router.route("/categories/user/:id").get((req, res) => {
+  Note.distinct("category", { userID: req.params.id })
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/categories/:category').get((req, res) => {
-  console.log(req.query.userID)
-  Note.find({userID: req.query.userID, category: req.params.category})
-  .then(notes => res.json(notes))
-  .catch(err => res.status(400).json('Error: ' + err));    
+router.route("/categories/:category").get((req, res) => {
+  Note.find({ userID: req.query.userID, category: req.params.category })
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const title = req.body.title;
   const content = req.body.content;
   const category = req.body.category;
@@ -74,37 +75,39 @@ router.route('/add').post((req, res) => {
     date,
   });
 
-  newNote.save()
-  .then(() => res.json(newNote._id))
-  .catch(err => res.status(400).json('Error: ' + err));
+  newNote
+    .save()
+    .then(() => res.json(newNote._id))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/:id').delete((req, res) => {
-Note.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Note deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/:id").delete((req, res) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Note deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/:id').get((req, res) => {
+router.route("/:id").get((req, res) => {
   Note.findById(req.params.id)
-    .then(note => res.json(note))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then((note) => res.json(note))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/update/:id').post((req, res) => {
-Note.findById(req.params.id)
-    .then(note => {
-    note.title = req.body.title;
-    note.content = req.body.content;
-    note.category = req.body.category;
-    note.tags = req.body.tags;
-    note.date = Date.parse(req.body.date);
+router.route("/update/:id").post((req, res) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      note.title = req.body.title;
+      note.content = req.body.content;
+      note.category = req.body.category;
+      note.tags = req.body.tags;
+      note.date = Date.parse(req.body.date);
 
-    note.save()
-        .then(() => res.json('Note updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
+      note
+        .save()
+        .then(() => res.json("Note updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
     })
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 //  DANGEROUS - FOR TRANSFERRING FROM ACCOUNTS ONLY
@@ -117,7 +120,7 @@ Note.findById(req.params.id)
 //         note.save()
 //           .then(note => res.json(note))
 //           .catch(err => res.status(400).json('Error: ' + err));
-//         });           
+//         });
 //       })
 //     .catch(err => res.status(400).json('Error: ' + err));
 // });
--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,118 +1,135 @@
-import CardView from './CardView';
-import {getCategoryNotes, getAllNotes} from '../utils/api';
-import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import styled from 'styled-components';
-import Button from './Atoms/Button';
-import axios from 'axios';
-import {useHistory} from 'react-router-dom';
-import ClickableIcon from './Atoms/ClickableIcon';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes, faFilter} from '@fortawesome/free-solid-svg-icons';
+import CardView from "./CardView";
+import { getCategoryNotes, getAllNotes } from "../utils/api";
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import styled from "styled-components";
+import Button from "./Atoms/Button";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import ClickableIcon from "./Atoms/ClickableIcon";
+import { faTimes, faFilter } from "@fortawesome/free-solid-svg-icons";
 
 const SectionText = styled.h5`
-    margin-top: 20px;
-    text-transform: uppercase;
-    font-size: 12px;
-    color: gray;
+  margin-top: 20px;
+  text-transform: uppercase;
+  font-size: 12px;
+  color: gray;
 `;
 
 const InputField = styled.input`
-    border-radius: 3px;
-    border: none;
-    padding: 5px;
-    background-color: #f0f2f5;
-    @media(max-width: 768px){
-        min-width: 0;
-    }
-    :focus {
-        outline: none;
-    }
+  border-radius: 3px;
+  border: none;
+  padding: 5px;
+  background-color: #f0f2f5;
+  @media (max-width: 768px) {
+    min-width: 0;
+  }
+  :focus {
+    outline: none;
+  }
 `;
 
 const Toolbar = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    @media(max-width: 768px) {
-        flex-direction: column;
-        flex-wrap:wrap;
-        align-items: start;
-    }
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: start;
+  }
 `;
 
 const CategoryPage = (props) => {
-    const user = props.user;
-    const category = window.location.pathname.split("/").pop();
-    const [notes, setNotes] = useState();
-    const [filter, setFilter] = useState();
-    const [filteredState, setFilteredState] = useState(false);
-    const history = useHistory();
+  const user = props.user;
+  const category = window.location.pathname.split("/").pop();
+  const [notes, setNotes] = useState();
+  const [filter, setFilter] = useState();
+  const [filteredState, setFilteredState] = useState(false);
+  const history = useHistory();
 
-    const routeChange = (route) =>{ 
-        let path = `${route}`; 
-        history.push(path);
+  const routeChange = (route) => {
+    let path = `${route}`;
+    history.push(path);
+  };
+
+  useEffect(() => {
+    if (filteredState === false && user) {
+      if (category === "All") {
+        getAllNotes(user._id)
+          .then((res) => setNotes(res))
+          .catch((err) => console.log(err));
+      } else {
+        getCategoryNotes(category, user._id)
+          .then((res) => setNotes(res))
+          .catch((err) => console.log(err));
+      }
     }
+  }, [filteredState]);
 
-    useEffect(() => {
-        if(filteredState === false && user) {
-            if(category === "All") {
-                getAllNotes(user._id)
-                .then(res => setNotes(res))
-                .catch(err => console.log(err))
-            }
-            else {
-                getCategoryNotes(category, user._id)
-                    .then(res => setNotes(res))
-                    .catch(err => console.log(err))            
-            }            
-        } 
-    }, [filteredState])
+  const handleNewNote = () => {
+    if (category != "All") routeChange(`/new/?category=${category}`);
+    else routeChange(`/new/`);
+  };
 
-    const handleNewNote = () => {
-        if (category != "All") routeChange(`/new/?category=${category}`);
-        else routeChange(`/new/`);
-    }
+  const handleFilter = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `api/notes/filterByTag/${user._id}/?tag=${filter}&category=${category}`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setNotes(res.data);
+      })
+      .catch((err) => console.log(err));
+    setFilteredState(true);
+  };
 
-    const handleFilter = (e) => {
-        e.preventDefault();
-        axios.get(`api/notes/filterByTag/?tag=${filter}&category=${category}`)
-          .then(res => {
-            setNotes(res.data);
-          })
-          .catch(err => console.log(err));
-        setFilteredState(true);
-    }
+  const resetFilter = () => {
+    setFilter("");
+    setFilteredState(false);
+  };
 
-    const resetFilter = () => {
-        setFilter("");
-        setFilteredState(false);
-    }
-
-    return (
-        <Container className="p-4">
-            <SectionText>CATEGORY</SectionText>
-            <h1>{category}</h1>
-            <Toolbar>
-                <Button onClick={handleNewNote} value={"New Note"}/>
-                <form onSubmit={handleFilter} className="mt-4 mb-4">
-                    <InputField className="mr-3" type="text" placeholder="Filter by tag" value={filter} onChange={(e) => setFilter(e.target.value)}/>
-                    <button type="submit" style={{all: "initial"}}>
-                        <ClickableIcon margin={filteredState ? true : false} onClick={null} color="#3f51b5" icon={faFilter}/>
-                    </button>
-                    {filteredState &&
-                        <button type="reset" onSubmit={resetFilter} onClick={resetFilter} style={{all: "initial"}}>
-                            <ClickableIcon color="#e91e63" icon={faTimes}/>
-                        </button>
-                    }
-                </form>                    
-            </Toolbar>
-            {notes && <CardView notes={notes}/>}                
-        </Container>
-
-    )
-}
+  return (
+    <Container className="p-4">
+      <SectionText>CATEGORY</SectionText>
+      <h1>{category}</h1>
+      <Toolbar>
+        <Button onClick={handleNewNote} value={"New Note"} />
+        <form onSubmit={handleFilter} className="mt-4 mb-4">
+          <InputField
+            className="mr-3"
+            type="text"
+            placeholder="Filter by tag"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          <button type="submit" style={{ all: "initial" }}>
+            <ClickableIcon
+              margin={filteredState ? true : false}
+              onClick={null}
+              color="#3f51b5"
+              icon={faFilter}
+            />
+          </button>
+          {filteredState && (
+            <button
+              type="reset"
+              onSubmit={resetFilter}
+              onClick={resetFilter}
+              style={{ all: "initial" }}
+            >
+              <ClickableIcon color="#e91e63" icon={faTimes} />
+            </button>
+          )}
+        </form>
+      </Toolbar>
+      {notes && <CardView notes={notes} />}
+    </Container>
+  );
+};
 
 export default CategoryPage;
