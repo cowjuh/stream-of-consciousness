7a3ac3067133a947161447ac598420b5fff1b50f
Jenny Zhang <jenzhang530@gmail.com>
2023-01-10 17:50:37 -0800

    Update oauth client id to use env

--- frontend/src/components/Authentication/GoogleLoginButton.js
+++ frontend/src/components/Authentication/GoogleLoginButton.js
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
-import {GoogleLogin} from 'react-google-login';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { GoogleLogin } from "react-google-login";
+import styled from "styled-components";
 
-const clientId = '979808801196-vn8rcr3df9c1qtgm1adfo4geksn3sioi.apps.googleusercontent.com';
+const clientId = process.env.OAUTH_CLIENT_ID;
 
 const LoginButton = styled(GoogleLogin)`
-    background-color: aqua;
+  background-color: aqua;
 `;
 
 const GoogleLoginButton = (props) => {
-    const [signedIn, setSignedIn] = useState(false);
-    const onSuccess = (res) => {
-        props.setUser(res.profileObj);
-        setSignedIn(true);
-    };
+  const [signedIn, setSignedIn] = useState(false);
+  const onSuccess = (res) => {
+    props.setUser(res.profileObj);
+    setSignedIn(true);
+  };
 
-    const onFailure = (res) => {
-        console.log('[Login failed] res: ', res);
-    };
+  const onFailure = (res) => {
+    console.log("[Login failed] res: ", res);
+  };
 
-    return (
-        <div>
-            <GoogleLogin
-                clientId={clientId}
-                buttonText="Sign in with Google"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={signedIn}
-                icon={true}
-                style={{color: "aqua"}}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Sign in with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={signedIn}
+        icon={true}
+        style={{ color: "aqua" }}
+      />
+    </div>
+  );
+};
 
 export default GoogleLoginButton;
