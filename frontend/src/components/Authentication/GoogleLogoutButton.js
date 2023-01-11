efee3ad0163121d337c64180b7d477394713a436
Jenny Zhang <jenzhang530@gmail.com>
2023-01-11 15:15:51 -0800

    Remove unused component

--- frontend/src/components/Authentication/GoogleLogoutButton.js
+++ frontend/src/components/Authentication/GoogleLogoutButton.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import {GoogleLogout} from 'react-google-login';
+import React from "react";
+import { GoogleLogout } from "react-google-login";
 
-const clientId = '979808801196-vn8rcr3df9c1qtgm1adfo4geksn3sioi.apps.googleusercontent.com';
+const clientId = process.env.REACT_APP_OAUTH_CLIENT_ID;
 
 const GoogleLogoutButton = (props) => {
-    const onSuccess = (res) => {
-        console.log('Logout made successfully');
-        props.handleLogout();
-    };
+  const onSuccess = () => {
+    console.log("Logout made successfully");
+    props.handleLogout();
+  };
 
-    return (
-        <div>
-            <GoogleLogout
-                clientId={clientId}
-                buttonText="Log Out"
-                onLogoutSuccess={onSuccess}
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <GoogleLogout clientId={clientId} buttonText="Log Out" onLogoutSuccess={onSuccess} />
+    </div>
+  );
+};
 
 export default GoogleLogoutButton;
