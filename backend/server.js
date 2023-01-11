1882625371a4dfd2692048d4dc37f4d063a3ac3d
Jenny Zhang <jenzhang530@gmail.com>
2023-01-10 16:13:22 -0800

    Update package.json

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const path = require('path');
-const bodyParser = require('body-parser');
-require('dotenv').config({path: __dirname + '/.env'});
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: __dirname + "/.env" });
 
 const app = express();
 
-const publicPath = path.join(__dirname, '../', '/build');
+const publicPath = path.join(__dirname, "../", "/build");
 const uri = process.env.ATLAS_URI;
-const notesRouter = require('./routes/notes');
-const usersRouter = require('./routes/users');
+const notesRouter = require("./routes/notes");
+const usersRouter = require("./routes/users");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/notes', notesRouter);
-app.use('/api/users', usersRouter);
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
 
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, '/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "/index.html"));
 });
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
-
-})
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
